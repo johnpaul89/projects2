0180f30f12f4c0286399a026fe1422dc1a3c2ecd
JoPa <johnpaulgitata@gmail.com>
2018-04-29 17:06:26 +0300

    Finished adding JavaScript of the icon

--- js/styles.js
+++ js/styles.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 
    // Close the dropdown menu if the user clicks outside of it
    window.onclick = function(event) {
+     
      if (!event.target.matches('.dropbtn')) {
 
        var dropdowns = document.getElementsByClassName("dropdown-content");
